059fee6ce0f5b444ce4c2f8581b16dd84a67cd73
Gabriel <gabrielbarcelos807@gmail.com>
2025-03-11 08:45:10 -0300

    verificar se o campo não está vazio

--- app.js
+++ app.js
@@ -2,4 +2,9 @@ let amigos = [];
 function adicionarAmigo() {
     
     const name = document.getElementById('input-name').value.trim();
-}
+
+    if (nome === "") {
+        alert("Por favor, digite um nome!");
+        return;
+      }
+    }
